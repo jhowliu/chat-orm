01e4b63b98bc9dc2003c3d4895ed78ab8b1ab708
Jhow <jhowliu@gmail.com>
2017-12-05 16:05:23 +0800

    update config

--- engine.py
+++ engine.py
@@ -6,8 +6,8 @@ from sqlalchemy.orm import sessionmaker
 from config import *
 
 # [driver]://user:password@host/dbname
-engine = create_engine('mysql://{}:{}@192.168.10.14/{}?charset=utf8' \
-            .format(USERNAME, PASSWORD, DATABASE))
+engine = create_engine('mysql://{}:{}@{}/{}?charset=utf8' \
+            .format(USERNAME, HOSTNAME, PASSWORD, DATABASE))
 
 
 Session = sessionmaker(bind=engine)
