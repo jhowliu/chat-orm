2f35485031441398f5853728a1c0fc5ce332f4a6
Jhow <jhowliu@gmail.com>
2017-12-06 15:51:01 +0800

    add some operation

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
 # -*- coding=utf-8 -*-
+import datetime
+
 from schema import *
 from engine import sess
+
+from sqlalchemy.sql import func
 
 # QUERY STATEMENT
 class RobotHandler():
@@ -9,17 +13,17 @@ class RobotHandler():
         self._venderId = venderId
         self._sess = sess
 
-    def list_robots(self):
+    def get_robot_list(self):
         rows = sess.query(Robots).all()
         return map(lambda row: row.__dict__, rows)
 
-    def robot_info(self):
-        row = sess.query(Robots).filter(Robots.VenderId==self._venderId).one()
+    def get_robot_info(self):
+        row = sess.query(Robots).filter(Robots.VenderId == self._venderId).one()
         return row.__dict__
 
     def get_greeting_msg(self):
         row = sess.query(Robots.GreetingMsg) \
-                  .filter(Robots.VenderId==self._venderId).one()
+                  .filter(Robots.VenderId == self._venderId).one()
         if row.GreetingMsg:
             return row.GreetingMsg.splitlines()
         else:
@@ -27,7 +31,7 @@ class RobotHandler():
 
     def get_failed_msg(self):
         row = sess.query(Robots.FailedMsg) \
-                  .filter(Robots.VenderId==self._venderId).one()
+                  .filter(Robots.VenderId == self._venderId).one()
         if row.FailedMsg:
             return row.FailedMsg.splitlines()
         else:
@@ -42,31 +46,109 @@ class RobotHandler():
         q = sess.query(Histories)
 
         if deviceId:
-            print("UFC")
-            q = q.filter(Histories.DeviceId==deviceId)
+            q = q.filter(Histories.DeviceId == deviceId)
         else:
             q = q.join(Robots) \
-                 .filter(Robots.VenderId==self._venderId)
+                 .filter(Robots.VenderId == self._venderId)
 
         rows = q.order_by(Histories.CreateAt) \
                 .limit(50)
 
         return map(lambda row: row.__dict__, rows)
 
-    def save_histories(self, histories):
+    def get_qa_list(self):
+        """
+        Fetch all pair of question and answer
 
-        return
+        @ return {
+            group_id: {
+                question,
+                answer
+            },
+            group_id: {
+                question,
+                answer
+            }...
+        }
+        """
+        results = {}
+        rows = sess.query(Groups.GroupId, Questions.Content, Answers.Content) \
+                   .join(Robots) \
+                   .filter(Robots.VenderId == self._venderId) \
+                   .filter(Groups.GroupId  == Questions.GroupId) \
+                   .filter(Groups.GroupId  == Answers.GroupId) \
+                   .all()
+
+        for row in rows:
+            (group_id, question, ans) = row
+            results[row.GroupId] = {
+                "Question": question,
+                "Answer"  : ans
+            }
+
+        return results
+
+    def get_user_count(self, ndays=7):
+        """
+        Fetch statistic in the ndays
+
+        @return {
+            date: {
+                user_count,
+                question_count
+            },
+            date: {
+                user_count,
+                question_count
+            }...
+        }
+        """
+        results = {}
+        start_t = datetime.datetime.now()
+        start_t -= datetime.timedelta(days=ndays)
+
+        rows = sess.query(func.date(Histories.CreateAt), \
+                        func.count(func.distinct(Histories.DeviceId)), \
+                        func.count(Histories.id)) \
+                   .filter(Histories.CreateAt >= func.date(start_t)) \
+                   .group_by(func.date(Histories.CreateAt)) \
+                   .all()
+
+        for row in rows:
+            (date, user_cnt, q_cnt) = row
+            results[date] = {
+                "question_count": q_cnt,
+                "user_count": user_cnt
+            }
+
+        return results
+
+    def save_qa(self, qas):
+        """
+        Save the Q&A
+
+        @param {
+            group_id: {
+                question
+                answer
+            }...
+        }
+
+        @return {
+            success: true
+        }
+        """
 
 
 if __name__ == '__main__':
     handler = RobotHandler('HVC');
 
     print('\n===LIST ROBOTS===\n')
-    rows = handler.list_robots()
+    rows = handler.get_robot_list()
     print(rows)
 
     print('\n===LIST ROBOT INFO===\n')
-    row = handler.robot_info()
+    row = handler.get_robot_info()
     print(row)
 
     print('\n===LIST GREETING MSG===\n')
@@ -74,3 +156,7 @@ if __name__ == '__main__':
 
     print('\n===LIST HISTORIES===\n')
     print(handler.get_histories())
+
+    print(handler.get_qa_list())
+
+    print(handler.get_user_count())
